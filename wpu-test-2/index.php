ec5fed26c49f873b075fb1e6d6d5c9d6252469ad
fajarsidik <sidik521@gmail.co.id>
2019-08-14 19:34:32 +0700

    menambahkan paragraf

--- wpu-test-2/index.php
+++ wpu-test-2/index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
    <h1>helo word</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident animi autem vel placeat, nobis modi quae consectetur quo. Ex alias similique cum sint odit quidem illo, consequatur eius consequuntur! Architecto.</p>
 </body>
 </html>
